242e1107ad1d6a43b6867712d57cd6b9556c2639
unknown <mouslimbouabdallah@gmail.com>
2024-12-15 14:57:25 +0100

    initial commit

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-!hi51pxtpg8m3oylk56ye^f&2ph_h)xrp@sg8n7uetqoskc@p6')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) ==  "1"
